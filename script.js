c7e0974c95e1a4b4b9d5b7806c6aa1b2ed84f120
Fayfen <92761007+Fayfen@users.noreply.github.com>
2021-10-22 11:28:30 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function fun() {
   var x = document.getElementById('in').value;
   window.alert(x);
 };
+
+var a;
+
+function getName() {
+  //Get value from input field and save as "a"    
+  a = document.getElementById('username').value;
+  //create LS key with value of "a"
+  localStorage.setItem("uName",a);    
+  //automatically take user to page two.html
+  location.href = "two.html";
+};
+    
+function getUser() {      
+  var b = localStorage.getItem('uName');
+  document.getElementById('placeholder').innerHTML = "Hello" + b;    
+};
